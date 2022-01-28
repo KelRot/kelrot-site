8499a20687935e50d368b451378a118f738a04ff
KelRot <56571165+KelRot@users.noreply.github.com>
2022-01-28 11:35:08 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# kelrot-site
-Kelrot site.
+# test
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("logo").style.width = "200px";
+        document.getElementById("logo").style.width = "150px";
+        document.getElementById("navbar").style.margin = "30px 6px 0px 0px";
     } 
     else {
         document.getElementById("logo").style.width = "275px";
+        document.getElementById("navbar").style.margin = "140px 6px 0px 0px";
     }
 }
