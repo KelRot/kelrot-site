77e8165da6229c32a8449a70bab827970be5f717
KelRot <56571165+KelRot@users.noreply.github.com>
2022-02-02 18:16:24 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 0.5 || document.documentElement.scrollTop > 0.5) {
         document.getElementById("logo").style.width = "150px";
-        document.getElementById("navbar").style.margin = "30px 6px 0px 0px";
+        document.getElementById("navbar").style.margin = "60px 0px 0px 0px";
     } 
     else {
-        document.getElementById("logo").style.width = "275px";
-        document.getElementById("navbar").style.margin = "140px 6px 0px 0px";
+        document.getElementById("logo").style.width = "225px";
+        document.getElementById("navbar").style.margin = "100px 0px 0px 0px";
     }
 }
